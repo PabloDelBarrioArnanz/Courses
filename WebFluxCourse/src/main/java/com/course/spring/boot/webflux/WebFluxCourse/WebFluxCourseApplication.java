86534f2cb9dd25923de947a24e083fc02917f780
PabloDelBarrio <b4eso03pbarrio@gmail.com>
2021-04-12 12:10:05 +0200

    feat: starting webflux course

---
+++ WebFluxCourse/src/main/java/com/course/spring/boot/webflux/WebFluxCourse/WebFluxCourseApplication.java
@@ -0,0 +1,13 @@
+package com.course.spring.boot.webflux.WebFluxCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxCourseApplication.class, args);
+	}
+
+}
