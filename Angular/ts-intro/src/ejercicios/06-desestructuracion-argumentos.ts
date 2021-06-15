57e4ccf606181485206358fe260bb932a8bf5f61
PabloDelBarrio <b4eso03pbarrio@gmail.com>
2021-06-15 12:45:42 +0200

    feat: expanding angular course

---
+++ Angular/ts-intro/src/ejercicios/06-desestructuracion-argumentos.ts
@@ -0,0 +1,34 @@
+
+
+interface Producto {
+    desc: string,
+    precio: number
+}
+
+const telefono: Producto = {
+    desc: 'Nokia A1',
+    precio: 190
+}
+
+const tablet: Producto = {
+    desc: 'IPad Pro',
+    precio: 1200
+}
+
+function calculaImpuestoSobreVenta(productos: Producto[]): [number, number] {
+    let total = 0;
+
+    //productos.map(producto => producto.precio) Java way
+        //.forEach(precio => total += precio);
+
+    productos.forEach( ({precio}) => total += precio); //Desestructuracion de argumentos para recorrer solo lo que queremos
+    
+    return [total, total * .15];
+}
+
+const articulos: Producto[] = [telefono, tablet];
+
+const [total, impuesto] = calculaImpuestoSobreVenta(articulos);
+
+console.log('Total ', total);
+console.log('Impuesto ', impuesto);
