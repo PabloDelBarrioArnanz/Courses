c9d673d87d890da3f4f85b453b23b9284e2097e4
PabloDelBarrioArnanz <pbarrioarnanz@gmail.com>
2023-09-07 17:51:12 +0200

    rockTheJVM kotlin tutorial

--- kotlin/RockTheJVM/build.gradle.kts
+++ kotlin/RockTheJVM/build.gradle.kts
@@ -2,6 +2,7 @@
 val slf4jVersion: String by project
 val logbackVersion: String by project
 val kotlinCoroutinesVersion: String by project
+val arrowCoreVersion: String by project
 
 plugins {
     kotlin("jvm") version "1.9.10"
@@ -18,4 +19,5 @@ dependencies {
     testImplementation("org.slf4j:slf4j-simple:$slf4jVersion")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
+    implementation("io.arrow-kt:arrow-core:$arrowCoreVersion")
 }
