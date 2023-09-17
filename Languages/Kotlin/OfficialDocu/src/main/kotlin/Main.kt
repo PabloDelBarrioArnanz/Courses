a81bc0fe7d041a1e193eb375fd5d33287e3d3281
PabloDelBarrioArnanz <pbarrioarnanz@gmail.com>
2023-09-17 16:39:01 +0200

    kotlin official docu

--- Languages/Kotlin/OfficialDocu/src/main/kotlin/Main.kt
+++ Languages/Kotlin/OfficialDocu/src/main/kotlin/Main.kt
@@ -715,6 +715,7 @@ val mutableList = mutableListOf("")
 val result = if (mutableList is List<*>) {
     mutableList.forEach { println(it) } // The items are typed as `Any?`
 } else null
+
 fun handleStrings(list: MutableList<String>) {
     if (list is ArrayList) {
         // `list` is smart-cast to `ArrayList<String>`
@@ -863,4 +864,179 @@ val securePassword = Password("Don't try this in production")
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-// Object expressions and declarations
+// Objects
+
+// Object as expressions and declarations
+// Creates objects of anonymous classes with object keyword
+val helloWorld = object {
+    val hello = "hello"
+
+    override fun toString(): String = "This is an overridden toString method"
+}
+
+// Also an anonymous object can inherit from other classes
+/*
+    window.addMouseListener(object : MouseAdapter() { // the object it's implementing MouseAdapter
+        override fun mouseClicked(e: MouseEvent) {}
+
+        override fun mouseEntered(e: MouseEvent) {}
+    })
+*/
+
+open class A(x: Int) {
+    open val y: Int = x
+}
+
+interface B
+
+val ab: A = object : A(1), B { // Anonymous extending A with properties and implementing B
+    override val y = 15
+}
+
+// An object can be returned from a function
+// If it's local or private but not inline declaration all members are accessible
+class C {
+    private fun getObject() = object { // Return type it's Any
+        val x: String = "x"
+    }
+
+    fun printX() {
+        println(getObject().x)
+    }
+}
+
+interface ToImplement {
+    fun toImplementMethod()
+}
+
+class NewC {
+    // The return type is Any; x is not accessible
+    fun getObject() = object {
+        val x: String = "x"
+    }
+
+    // The return type is A; x is not accessible
+    fun getObjectA() = object : ToImplement {
+        override fun toImplementMethod() {}
+        val x: String = "x"
+    }
+
+    // The return type is B or ToImplement we can choose; funFromA() and x are not accessible
+    fun getObjectB(): B = object : ToImplement, B { // explicit return type is required
+        override fun toImplementMethod() {}
+        val x: String = "x"
+    }
+}
+
+// Accessing to private and public
+class Wrapper {
+    private var privateCounter = 0
+    var publicCounter = 0
+
+    val myObject = object {
+        fun increment() {
+            privateCounter++
+            publicCounter++
+        }
+    }
+}
+
+// Object as declarations
+// The singleton pattern it's easy to declare with objects
+// In this case in not an expression
+object DataProvider {
+    fun registerData() {}
+}
+
+val provider = DataProvider.registerData()
+
+// Data objects
+data object MyDataObject {
+    val x = "text"
+}
+
+// Data objects has implemented the equals and toString method
+// But it's better to compare it with == instance of === bcs can fail if there are more than one instance created in runtime
+// This is only possible cheating for example reflection
+
+// Data Objects has no copy neither componentN like data classes
+
+// Companion Objects
+class ClassWithCompanionNamed {
+    companion object Factory { // Name can be omitted
+        fun create(): ClassWithCompanionNamed = ClassWithCompanionNamed()
+    }
+}
+val instance = ClassWithCompanionNamed.create()
+
+class ClassWithCompanionNotNamed {
+    companion object {
+        fun create(): ClassWithCompanionNotNamed = ClassWithCompanionNotNamed()
+    }
+}
+
+// With name or not the class name referred to companion object
+val x = ClassWithCompanionNamed
+val y = ClassWithCompanionNotNamed
+
+// Companion objects can implement interfaces
+
+interface Factory<T> {
+    fun create(): T
+}
+
+class MyClassFactory {
+    companion object : Factory<MyClassFactory> {
+        override fun create(): MyClassFactory = MyClassFactory()
+    }
+}
+
+val f: Factory<MyClassFactory> = MyClassFactory
+
+// Object expressions are executed an initialized immediately
+// Object declarations are initialized lazily (when accessed for the first time)
+//      Companion object is initialized when the corresponding class is loaded
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Delegation
+// This pattern it's a good alternative to implementation inheritance
+// A class Derived can implement an interface Base by delegating all of its public members to a specified object
+
+// Normal interface
+interface Base {
+    fun printMessage()
+    fun printMessageLine()
+}
+
+// Normal class implementing interface
+class BaseImpl(val x: Int) : Base {
+    override fun printMessage() { print(x) }
+    override fun printMessageLine() { println(x) }
+}
+
+// Class witch instance of inherit from BaseImpl derived it and can redefine or not any methods
+class Derived(b: Base) : Base by b {
+    override fun printMessage() { print("abc") }
+}
+
+fun tryIt() {
+    val b = BaseImpl(10)
+    Derived(b).printMessage()
+    Derived(b).printMessageLine()
+}
+
+// Other example
+interface ClosedShape {
+    fun area(): Int
+}
+
+class RectangleShape(val width: Int, val height: Int) : ClosedShape {
+    override fun area() = width * height
+}
+
+// When a window it's created any implementation of CloseShape can be passed as parameter
+class Window(private val bounds: ClosedShape) : ClosedShape by bounds
+
+// Delegated properties
