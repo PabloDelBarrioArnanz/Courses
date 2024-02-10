b97b7fc71db230bd82565e28bc64f940643ae381
PabloDelBarrioArnanz <pbarrioarnanz@gmail.com>
2024-02-10 21:27:10 +0100

    mouredev JetPack tutorial

--- Languages/Kotlin/Frameworks/JetPack/MoureDevJetPackTutorial/app/src/main/java/com/pablodelbarrio/mouredevjetpacktutorial/DisplayList.kt
+++ Languages/Kotlin/Frameworks/JetPack/MoureDevJetPackTutorial/app/src/main/java/com/pablodelbarrio/mouredevjetpacktutorial/DisplayList.kt
@@ -6,18 +6,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -58,6 +61,13 @@ fun MyList() {
 
 @Composable
 fun MyListItem() {
+
+    /*
+        A normal var when it's update the jetpack compose doesn't watch it
+        to indicate it's a variable witch his value can change an make the ui refresh we have to init it like this
+    */
+    var expandedRow by remember { mutableStateOf(false) }
+
     Row(
         modifier = Modifier
             .background(MaterialTheme.colorScheme.background)
@@ -73,10 +83,18 @@ fun MyListItem() {
                 .background(MaterialTheme.colorScheme.primary)
         )
         Column(
-            modifier = Modifier.padding(8.dp)
+            modifier = Modifier
+                .padding(8.dp)
+                .clickable {
+                    expandedRow = !expandedRow
+                }
         ) {
             Text(text = "Hello JetPack Compose!", color = MaterialTheme.colorScheme.primary)
-            Text(text = "Are you ready!?", color = MaterialTheme.colorScheme.secondary)
+            Text(
+                text = "Are you ready!? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book".trimMargin(),
+                color = MaterialTheme.colorScheme.secondary,
+                maxLines = if (expandedRow) Integer.MAX_VALUE else 1 // max lines displayed
+            )
         }
     }
 }
