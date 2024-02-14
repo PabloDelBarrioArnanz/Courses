c685d0940fa5128986811abf6b310f45ced0301f
PabloDelBarrioArnanz <pbarrioarnanz@gmail.com>
2024-02-14 19:57:52 +0100

    jetpack compose definitive course

--- Languages/Kotlin/Frameworks/JetPack/JetPackComposeDefinitiveCourseUdemy/app/src/main/java/com/pablodelbarrio/jetpackcomposedefinitivecourseudemy/MainActivity.kt
+++ Languages/Kotlin/Frameworks/JetPack/JetPackComposeDefinitiveCourseUdemy/app/src/main/java/com/pablodelbarrio/jetpackcomposedefinitivecourseudemy/MainActivity.kt
@@ -3,12 +3,16 @@ package com.pablodelbarrio.jetpackcomposedefinitivecourseudemy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.pablodelbarrio.jetpackcomposedefinitivecourseudemy.ui.theme.JetPackComposeDefinitiveCourseUdemyTheme
 
@@ -33,11 +37,23 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
+        // With the modifier we can change any component properties, like height, color, clickable behaviour...
         modifier = modifier
+            .background(Color.Gray)
+            .fillMaxSize()
+            .padding()
+            .clickable {
+
+            }
     )
 }
 
-@Preview(showBackground = true)
+/*
+    Limitations the preview function can't receive parameters
+    We can have multiple previews to
+    The preview can receive many parameters like show background, showSystemUi, device to test...
+*/
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     JetPackComposeDefinitiveCourseUdemyTheme {
