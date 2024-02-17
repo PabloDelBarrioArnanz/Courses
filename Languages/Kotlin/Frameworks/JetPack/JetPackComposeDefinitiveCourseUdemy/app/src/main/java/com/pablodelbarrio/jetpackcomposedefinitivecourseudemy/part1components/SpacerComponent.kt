5ca3eed6a7d88381c580429cd21e2d0524e489a7
PabloDelBarrioArnanz <pbarrioarnanz@gmail.com>
2024-02-17 19:48:23 +0100

    jetpack compose definitive course

---
+++ Languages/Kotlin/Frameworks/JetPack/JetPackComposeDefinitiveCourseUdemy/app/src/main/java/com/pablodelbarrio/jetpackcomposedefinitivecourseudemy/part1components/SpacerComponent.kt
@@ -0,0 +1,46 @@
+package com.pablodelbarrio.jetpackcomposedefinitivecourseudemy.part1components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MySpacer() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+    ) {
+        Text(
+            text = "Text 1", modifier = Modifier
+                .background(Color.Red)
+                .fillMaxWidth()
+                .height(100.dp)
+        )
+        // It's a empty space
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = "Text 2", color = Color.White, modifier = Modifier
+                .background(Color.Black)
+                .fillMaxWidth()
+                .height(100.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun SpacerPreview() {
+    MySpacer()
+}
