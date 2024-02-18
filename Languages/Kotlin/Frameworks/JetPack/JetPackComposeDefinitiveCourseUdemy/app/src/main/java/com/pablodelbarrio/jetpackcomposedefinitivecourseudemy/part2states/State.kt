43597740a69dd967dba0e007a0aefe3c2854e5d5
PabloDelBarrioArnanz <pbarrioarnanz@gmail.com>
2024-02-18 12:38:20 +0100

    jetpack compose definitive course

--- Languages/Kotlin/Frameworks/JetPack/JetPackComposeDefinitiveCourseUdemy/app/src/main/java/com/pablodelbarrio/jetpackcomposedefinitivecourseudemy/part2states/State.kt
+++ Languages/Kotlin/Frameworks/JetPack/JetPackComposeDefinitiveCourseUdemy/app/src/main/java/com/pablodelbarrio/jetpackcomposedefinitivecourseudemy/part2states/State.kt
@@ -5,13 +5,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,33 +25,48 @@ fun MyState() {
         - Mutable state wraps a value and triggers a view refresh when his value it's update
         - Remember makes that part of code don't change between refresh due in each refresh this function
           will be called then that part will restore the last value
+
+        - Be careful with the life cycle of the Android app, when the app it's refreshing it self remember works
+          but when the app it's destroy and recreated from zero remember can't do nothing we need to use rememberSaveable
+          (for example when the phone rotates the screen the complete activity will be destroyed)
     */
-    val counter = remember { mutableIntStateOf(0) }
+
+    // val counter = remember { mutableIntStateOf(0) }
+    // val counter = rememberSaveable { mutableIntStateOf(0) }
+
+    // by makes initialize the state as = but makes his value accessible directly with no counter.intValue...
+    // Needs to be var
+    var counter by rememberSaveable { mutableIntStateOf(0) }
 
     Column(
         Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Clicked ${counter.intValue} times")
+        Text(text = "Clicked $counter times")
         Row(
-            Modifier.height(80.dp).width(400.dp),
+            Modifier
+                .height(80.dp)
+                .width(400.dp),
             horizontalArrangement = Arrangement.SpaceAround,
             verticalAlignment = Alignment.CenterVertically
         ) {
             Button(onClick = {
-                counter.intValue++
+                // counter.intValue++
+                counter = counter.inc()
             }) {
                 Text(text = "Add Click!")
             }
             Button(onClick = {
-                counter.intValue--
+                // counter.intValue--
+                counter = counter.dec()
             }) {
                 Text(text = "Decrees Click!")
             }
         }
         Button(onClick = {
-            counter.intValue = 0
+            // counter.intValue = 0
+            counter = counter.minus(counter)
         }) {
             Text(text = "Reset Click!")
         }
