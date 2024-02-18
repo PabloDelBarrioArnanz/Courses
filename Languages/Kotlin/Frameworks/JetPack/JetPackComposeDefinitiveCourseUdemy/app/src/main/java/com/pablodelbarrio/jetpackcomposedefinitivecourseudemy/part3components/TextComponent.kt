cf59945284dca77e955c85291c4bd40d3588fee4
PabloDelBarrioArnanz <pbarrioarnanz@gmail.com>
2024-02-18 13:09:12 +0100

    jetpack compose definitive course

--- Languages/Kotlin/Frameworks/JetPack/JetPackComposeDefinitiveCourseUdemy/app/src/main/java/com/pablodelbarrio/jetpackcomposedefinitivecourseudemy/part3components/TextComponent.kt
+++ Languages/Kotlin/Frameworks/JetPack/JetPackComposeDefinitiveCourseUdemy/app/src/main/java/com/pablodelbarrio/jetpackcomposedefinitivecourseudemy/part3components/TextComponent.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
