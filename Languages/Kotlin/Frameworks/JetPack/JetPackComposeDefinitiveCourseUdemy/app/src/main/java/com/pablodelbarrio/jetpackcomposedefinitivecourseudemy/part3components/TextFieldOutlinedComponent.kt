c39604368169efb3ef7f9c323d372187bbf1a2ed
PabloDelBarrioArnanz <pbarrioarnanz@gmail.com>
2024-02-18 13:08:15 +0100

    jetpack compose definitive course

--- Languages/Kotlin/Frameworks/JetPack/JetPackComposeDefinitiveCourseUdemy/app/src/main/java/com/pablodelbarrio/jetpackcomposedefinitivecourseudemy/part3components/TextFieldOutlinedComponent.kt
+++ Languages/Kotlin/Frameworks/JetPack/JetPackComposeDefinitiveCourseUdemy/app/src/main/java/com/pablodelbarrio/jetpackcomposedefinitivecourseudemy/part3components/TextFieldOutlinedComponent.kt
@@ -1,9 +1,6 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.pablodelbarrio.jetpackcomposedefinitivecourseudemy.part3components
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
