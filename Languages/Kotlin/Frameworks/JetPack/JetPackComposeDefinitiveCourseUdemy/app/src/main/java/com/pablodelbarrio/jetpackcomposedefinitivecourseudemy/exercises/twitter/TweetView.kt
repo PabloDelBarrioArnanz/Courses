4a65540282e229db32bde1cc609f10ce033284cd
PabloDelBarrioArnanz <pbarrioarnanz@gmail.com>
2024-03-26 21:06:41 +0100

    jetpack compose definitive course

---
+++ Languages/Kotlin/Frameworks/JetPack/JetPackComposeDefinitiveCourseUdemy/app/src/main/java/com/pablodelbarrio/jetpackcomposedefinitivecourseudemy/exercises/twitter/TweetView.kt
@@ -0,0 +1,15 @@
+package com.pablodelbarrio.jetpackcomposedefinitivecourseudemy.exercises.twitter
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TweetView() {
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun TweetViewPreview() {
+    TweetView()
+}
