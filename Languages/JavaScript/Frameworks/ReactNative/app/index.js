3d01bbd3df34044249b4319198abc3b2fcdf37f2
Pablo del Barrio <pbarrioarnanz@gmail.com>
2026-02-23 20:37:54 +0100

    midudev react-native

--- Languages/JavaScript/Frameworks/ReactNative/app/index.js
+++
@@ -1,7 +0,0 @@
-import { Main } from "../components/Main";
-
-import "../global.css";
-
-export default function Index() {
-  return <Main />;
-}
--- Languages/JavaScript/Frameworks/ReactNative/components/Screen.jsx
+++
@@ -1,9 +0,0 @@
-import { View } from "react-native";
-
-export function Screen({ children }) {
-  return (
-    <View className="flex-1 bg-black mt-4">
-      {children}
-    </View>
-  )
-}
