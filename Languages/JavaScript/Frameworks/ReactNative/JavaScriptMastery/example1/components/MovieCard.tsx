e65b7fd3d63fe2f46f61ef988b072d0a1a0a6e85
Pablo del Barrio <pbarrioarnanz@gmail.com>
2026-02-27 18:27:50 +0100

    javascript mastery react-native course

--- Languages/JavaScript/Frameworks/ReactNative/JavaScriptMastery/example1/components/MovieCard.tsx
+++ Languages/JavaScript/Frameworks/ReactNative/JavaScriptMastery/example1/components/MovieCard.tsx
@@ -1,5 +1,6 @@
+import { icons } from "@/constants/icons";
 import { Link } from "expo-router";
-import { Image, Text, TouchableOpacity } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 const MovieCard = ({
   id,
@@ -9,7 +10,7 @@ const MovieCard = ({
   release_date,
 }: Movie) => {
   return (
-    <Link href={`/movie/${id}`} asChild>
+    <Link href={`/movies/${id}`} asChild>
       <TouchableOpacity className="w-[30%]">
         <Image
           source={{
@@ -24,6 +25,19 @@ const MovieCard = ({
         <Text className="text-sm font-bold text-white mt-2" numberOfLines={1}>
           {title}
         </Text>
+
+        <View className="flex-row items-center justify-start gap-x-1">
+          <Image source={icons.star} className="size-4" />
+          <Text className="text-xs text-white font-bold uppercase">
+            {Math.round(vote_average / 2)}
+          </Text>
+        </View>
+
+        <View className="flex-row items-center justify-between">
+          <Text className="text-xs text-light-300 fond-medium mt-1">
+            {release_date?.split("-")[0]}
+          </Text>
+        </View>
       </TouchableOpacity>
     </Link>
   );
